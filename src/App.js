19392ccd83334733e0da3c44561e28e3e6b7d069
nkbadhon <badhon7574@gmail.com>
2022-09-29 10:27:56 +0600

    Sections added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Container from './Componenets/MainContainer/Container';
 
 function App() {
   return (
     <div className="App">
-
+      <Container></Container>
     </div>
   );
 }
---
+++ src/Componenets/Workout-Items/Items.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './items.css';
+const Items = () => {
+    return (
+        <div className="gym-items">
+            <h2 className='header'>BodyBuilder.com</h2>
+        </div>
+    );
+};
+
+export default Items;
